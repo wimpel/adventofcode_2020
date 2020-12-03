afbf86221bce13b7e5ada492f876c251d8dbae9e
wimbreedveld <wim.breedveld@gmail.com>
2020-12-03 19:46:18 +0100

    added part 2 <facepalm>

--- r/day01.R
+++ r/day01.R
@@ -1,5 +1,5 @@
 library( data.table )
-DT <- data.table::fread( "./data/day01.txt" )
+DT <- data.table::fread( "./data/day01-input.txt" )
 data.table::CJ( DT$V1, DT$V1 )[, sum := V1 + V2][ sum == 2020, ]
 # V1   V2  sum
 # 1:   21 1999 2020
@@ -7,3 +7,12 @@ data.table::CJ( DT$V1, DT$V1 )[, sum := V1 + V2][ sum == 2020, ]
 
 1999 * 21
 #[1] 41979
+
+####################
+# part 2
+####################
+DT <- data.table::fread( "./data/day01-input.txt" )
+combos <- as.data.table( t( combn( DT$V1, 3, simplify = TRUE ) ) )
+prod( combos[ rowSums( combos ) == 2020, ] )
+
+#193416912
